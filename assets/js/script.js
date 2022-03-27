7a79c0f31edfe1733c3d17196c8739b5678af9b1
ilkkantuluce <ilkkantuluce07@gamil.com>
2022-03-28 00:09:02 +0200

    progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,39 @@ function classToggle() {
       menu.className = "rightMenu";
     }
 }
+
+const nodeList = document.querySelectorAll(".circle");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.strokeDasharray = "0 100";
+}
+
+
+var element = document.getElementById('circle-wrap');
+var elementHeight = element.clientHeight;
+
+document.addEventListener('scroll', animate);
+
+function inView() {
+  var windowHeight = window.innerHeight;
+  var scrollY = window.scrollY || window.pageYOffset;
+  
+  var scrollPosition = scrollY + windowHeight;
+  var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
+  
+  if (scrollPosition > elementPosition) {
+    return true;
+  }
+  
+  return false;
+}
+
+function animate() {
+    if (inView()) {
+        setTimeout(function() {
+            element.classList.add('animate');
+            for (let i = 0; i < nodeList.length; i++) {
+                nodeList[i].style.strokeDasharray = "";
+            }
+        }, 100);
+    }
+}
